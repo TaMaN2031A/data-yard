5b11b530a78cf44111b6fabfd4e188b228d482de
TaMaN2031A <es-abdullahm.abdullah2025@alexu.edu.eg>
2025-11-10 20:06:19 +0200

    Contribution steps and ChatMessage example.

--- haystack/Concepts/Data Classes/ChatMessage/from_user with TextContent and ImageContent.py
+++ haystack/Concepts/Data Classes/ChatMessage/from_user with TextContent and ImageContent.py
@@ -1,18 +1,22 @@
+
 from haystack.dataclasses import ChatMessage, ImageContent
 
-capybara_image_url = (
-    "https://cdn.pixabay.com/photo/2017/04/24/11/05/capybara-2252556_960_720.jpg"
+lion_image_url = (
+    "https://images.unsplash.com/photo-1546182990-dffeafbe841d?"
+	"ixlib=rb-4.0&q=80&w=1080&fit=max"
 )
 
-image_content = ImageContent.from_url(url=capybara_image_url, detail="low")
+image_content = ImageContent.from_url(lion_image_url, detail="low")
+
 user_message = ChatMessage.from_user(
-    content_parts=[
-        "What does the image show?",
-        image_content
-    ]
-)
+	content_parts=[
+		"What does the image show?",
+		image_content
+		])
 
 print(user_message)
+
+
 print(user_message.text)
+
 print(user_message.texts)
-print(user_message.image)
