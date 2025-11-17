6fa99fae7878b127a8895b5148758c1fe872a4f7
TaMaN2031A <es-abdullahm.abdullah2025@alexu.edu.eg>
2025-11-17 02:34:24 +0200

    Writing the first DAG, not finding it ina airflow yet

--- Data Engineering with Python Work with massive datasets to design data models and automate data pipelines using Python (Crickard, Paul)/Chapter 3 Reading and Writing Files/Building data pipelines in Apache Airflow.py
+++ Data Engineering with Python Work with massive datasets to design data models and automate data pipelines using Python (Crickard, Paul)/Chapter 3 Reading and Writing Files/Building data pipelines in Apache Airflow.py
@@ -11,7 +11,7 @@ def csv_to_json():
 
 default_args = {
     'owner': 'airflow',
-    'start_date': dt.datetime(2025, 11, 11),
+    'start_date': dt.datetime(2025, 11, 17),
     'retries': 1,
     'retry_delay': dt.timedelta(minutes=5)
 }
@@ -20,3 +20,13 @@ with DAG(
     default_args=default_args,
     schedule=dt.timedelta(minutes=5),
 ) as dag:
+    print_starting = BashOperator(
+        task_id='starting',
+        bash_command='echo "I am reading the csv now"'
+    )
+    csvtojson_task = PythonOperator(
+        task_id='csvtojson',
+        python_callable=csv_to_json,
+    )
+    print_starting >> csvtojson_task
+
